c2d0280aa49e3733c00aade7387a957bd293c258
Wii-Yang <wii_yang@163.com>
2026-06-09 17:13:58 +0800

    feat: 增加 /cesium-map 前缀

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
+  base: '/cesium-map/',
   plugins: [
     vue(),
     cesium({ cesiumBaseUrl: '/cesium/' }),
