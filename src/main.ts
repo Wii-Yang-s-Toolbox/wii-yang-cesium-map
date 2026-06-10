b391a9d4bf7f180895de761129c825ea5bce1bed
Wii-Yang <wii_yang@163.com>
2026-06-10 11:58:01 +0800

    style: 样式文件调整为 less

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import '@/assets/styles/index.less'
 import App from './App.vue'
 import router from './router'
 import 'ant-design-vue/dist/reset.css'
