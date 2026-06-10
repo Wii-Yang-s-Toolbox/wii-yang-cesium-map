370960692751b24384a7adb728130b0aca0f0914
Wii-Yang <wii_yang@163.com>
2026-06-10 14:33:37 +0800

    chore: 配置 eslint-plugin-prettier 插件

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
 
 export default tseslint.config(
@@ -8,6 +9,7 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
   {
     files: ['*.vue', '**/*.vue'],
     languageOptions: {
